1882a73249965d8e9fa6b9c48e063f4634d91788
Saikumar Kusuma <saikumar.kusuma@gmail.com>
2020-06-14 22:31:55 +0530

    modified startup script

--- tools/start-script.js
+++ tools/start-script.js
@@ -1,25 +1,31 @@
 const child_process = require('child_process');
 
+/**
+ * @type {import('child_process').ChildProcess}
+ */
+let serveProcess = null;
+
+process.on('SIGINT', () => {
+  if (serveProcess) {
+    serveProcess.on('close', () => {
+      downDockerCompose();
+      process.exit(0);
+    });
+  } else {
+    downDockerCompose();
+    process.exit(0);
+  }
+});
+
 child_process.execSync('docker-compose up -d', {
   cwd: 'tools',
   stdio: 'inherit',
 });
 
-const serveProcess = child_process.exec('npm run serve', {
+serveProcess = child_process.exec('npm run serve', {
   env: { ...process.env, FORCE_COLOR: 'true' },
 });
 
-process.on('SIGINT', () => {
-  serveProcess.kill('SIGINT');
-  serveProcess.on('close', () => {
-    console.info('Stopping Docker containers. Please wait..');
-    child_process.execSync('docker-compose down', {
-      cwd: 'tools',
-      stdio: 'inherit',
-    });
-    process.exit(0);
-  });
-});
 serveProcess.stdout.on('data', (data) => {
   process.stdout.write(data);
 });
@@ -27,3 +33,11 @@ serveProcess.stdout.on('data', (data) => {
 serveProcess.stderr.on('data', (err) => {
   process.stderr.write(err);
 });
+
+function downDockerCompose() {
+  console.info('Stopping Docker containers. Please wait..');
+  child_process.execSync('docker-compose down', {
+    cwd: 'tools',
+    stdio: 'inherit',
+  });
+}
